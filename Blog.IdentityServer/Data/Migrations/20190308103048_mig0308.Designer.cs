6b26021ce2f8cca4fc014288c05afd49374a4434
anjoy8 <3143422472@qq.com>
2019-03-08 18:31:24 +0800

    init 2

--- Blog.IdentityServer/Data/Migrations/20190308103048_mig0308.Designer.cs
+++ Blog.IdentityServer/Data/Migrations/20190308103048_mig0308.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Blog.IdentityServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Blog.IdentityServer.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190308103048_mig0308")]
+    partial class mig0308
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Blog.IdentityServer/Data/Migrations/20190308103048_mig0308.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.IdentityServer.Data.Migrations
+{
+    public partial class mig0308 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
