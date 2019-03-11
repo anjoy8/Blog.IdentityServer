3fdde91a31afd7d9e2fd9a17d2c7786b0d32f4f1
anjoy8 <3143422472@qq.com>
2019-03-11 12:02:52 +0800

    ingore id4_log.txt

--- Blog.IdentityServer/Program.cs
+++ Blog.IdentityServer/Program.cs
@@ -22,7 +22,7 @@ namespace Blog.IdentityServer
                 .MinimumLevel.Override("System", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
                 .Enrich.FromLogContext()
-                .WriteTo.File(@"identityserver4_log.txt")
+                .WriteTo.File(@"Logs/identityserver4_log.txt")
                 .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
                 .CreateLogger();
 
