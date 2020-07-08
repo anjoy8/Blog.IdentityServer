6413043e43bdb59dd1f67aabd6e268762e0a66a5
anjoy8 <3143422472@qq.com>
2020-07-08 16:59:50 +0800

    Create Blog.IdentityServer.Publish.Docker.sh

---
+++ Blog.IdentityServer.Publish.Docker.sh
@@ -0,0 +1,18 @@
+﻿# 停止容器
+docker stop idscontainer
+# 删除容器
+docker rm idscontainer
+# 删除镜像
+docker rmi laozhangisphi/idsimg
+# 切换目录
+cd /home/Blog.IdentityServer
+# 发布项目
+./Blog.IdentityServer.Publish.Docker.sh
+# 进入目录
+cd /home/Blog.IdentityServer/.PublishFiles/
+# 编译镜像
+docker build -t laozhangisphi/idsimg .
+# 生成容器
+docker run --name=idscontainer -v /etc/localtime:/etc/localtime -it -p 5004:5004 laozhangisphi/idsimg
+# 启动容器
+docker start apkcontainer
