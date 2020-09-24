aee23adc6cec6f6a58d78a44e2fd93ff87bab1ba
anjoy8 <3143422472@qq.com>
2020-09-24 18:08:12 +0800

    suport api

---
+++ Blog.IdentityServer/Controllers/Home/Is4ApiController.cs
@@ -0,0 +1,65 @@
+﻿using Blog.IdentityServer.Models;
+using Blog.IdentityServer.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.IdentityServer.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class Is4ApiController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public Is4ApiController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [HttpGet]
+        public MessageModel<AccessApiDateView> GetAchieveUsers()
+        {
+            List<ApiDate> apiDates = new List<ApiDate>();
+
+            var users = _userManager.Users.Where(d => !d.tdIsDelete).OrderByDescending(d => d.Id).ToList();
+
+            var tadayRegisterUser = users.Where(d => d.birth >= DateTime.Now.Date).Count();
+
+            apiDates = (from n in users
+                        group n by new { n.birth.Date } into g
+                        select new ApiDate
+                        {
+                            date = g.Key?.Date.ToString("yyyy-MM-dd"),
+                            count = g.Count(),
+                        }).ToList();
+
+            apiDates = apiDates.OrderByDescending(d => d.date).Take(30).ToList();
+
+
+            if (apiDates.Count == 0)
+            {
+                apiDates.Add(new ApiDate()
+                {
+                    date = "没数据,或未开启相应接口服务",
+                    count = 0
+                });
+            }
+            return new MessageModel<AccessApiDateView>()
+            {
+                msg = "获取成功",
+                success = true,
+                response = new AccessApiDateView
+                {
+                    columns = new string[] { "date", "count" },
+                    rows = apiDates.OrderBy(d => d.date).ToList(),
+                    today = tadayRegisterUser,
+                }
+            };
+        }
+    }
+}
---
+++ Blog.IdentityServer/Models/ViewModel/AccessApiDateView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.IdentityServer.Models.ViewModel
+{
+    public class AccessApiDateView
+    {
+        public int today { get; set; }
+        public string[] columns { get; set; }
+        public List<ApiDate> rows { get; set; }
+    }
+    public class ApiDate
+    {
+        public string date { get; set; }
+        public int count { get; set; }
+    }
+}
