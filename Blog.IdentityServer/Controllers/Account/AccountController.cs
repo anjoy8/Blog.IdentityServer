6291ca9303e9261f74698a5fa2ed3d8fbf6595d1
anjoy8 <3143422472@qq.com>
2020-11-02 18:22:54 +0800

    Update AccountController.cs

--- Blog.IdentityServer/Controllers/Account/AccountController.cs
+++ Blog.IdentityServer/Controllers/Account/AccountController.cs
@@ -113,7 +113,7 @@ namespace IdentityServer4.Quickstart.UI
                 var user = await _userManager.FindByNameAsync(model.Username);
 
 
-                if (!user.tdIsDelete)
+                if (user != null && !user.tdIsDelete)
                 {
                     var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberLogin, lockoutOnFailure: true);
                     if (result.Succeeded)
